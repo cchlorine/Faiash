ba7dc1c125d1fa5fc568e48b3c29fa6918537f3c
Rakume Hayashi <i@fake.moe>
2015-07-17 05:08:16 +0800

    Fix cannot access to Faiash

--- dist/faiash.js
+++ dist/faiash.js
@@ -170,5 +170,10 @@ Faiash.ise.extend({
 
 if (typeof module === "object" && typeof module.exports === "object") {
     moudle.exports = Faiash;
+} else {
+    window.Faiash = Faiash;
+    if (!window.$) {
+        window.$ = Faiash;
+    }
 }
 })();
--- src/outro.js
+++ src/outro.js
@@ -1,5 +1,10 @@
 
 if (typeof module === "object" && typeof module.exports === "object") {
     moudle.exports = Faiash;
+} else {
+    window.Faiash = Faiash;
+    if (!window.$) {
+        window.$ = Faiash;
+    }
 }
 })();
