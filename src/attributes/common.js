3b09b27a0efc470ca03e0b1a7ec78b2f6a7e02d1
Rakume Hayashi <i@fake.moe>
2015-07-17 05:46:50 +0800

    Add css

--- src/attributes/common.js
+++ src/attributes/common.js
@@ -25,5 +25,21 @@ Faiash.ise.extend({
         }
 
         return this;
+    },
+
+    css: function(v) {
+        if (!v) {
+            return this[0].style.cssText;
+        }
+
+        if (v.substr(-1) == ';') {
+            v = v.substr(0, v.length - 1);
+        }
+
+        for (var i = 0; i < this.length; i++) {
+            this.style.cssText += (';' + v);
+        }
+
+        return this.style.cssText;
     }
 });
