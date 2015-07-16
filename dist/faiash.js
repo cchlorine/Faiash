25fd3546cc216a2bf6690ec1cd08c11489f211ea
Rakume Hayashi <i@fake.moe>
2015-07-17 06:06:44 +0800

    Gulp

--- dist/faiash.js
+++ dist/faiash.js
@@ -29,6 +29,12 @@ var init = Faiash.ise.init = function(selector, context) {
         return this;
     }
 
+    if (selector === doucment) {
+        return document;
+    } else if (selector === window) {
+        return window;
+    }
+
     // When context is string
     if (typeof context == 'string') {
         context = document.querySelector(context);
@@ -75,97 +81,28 @@ Faiash.extend = Faiash.ise.extend = function() {
 }
 
 /**
- * Classes
+ * Event
  *
  * @author Rakume Hayashi<i@fake.moe>
  */
 
 Faiash.ise.extend({
-    addClass: function(v) {
-        // Return false when v is '', void, undefined, false or when v is not a string
-        if (!v || typeof v !== 'string') {
-            return false;
-        }
-
-        if (document.body.classList) {
+    on: function() {
+        if (typeof arguments[0] === 'string') {
             for (var i = 0; i < this.length; i++) {
-                this[i].classList.add(v);
+                this[i].addEventListener(arguments[0], arguments[1], arguments[2]);
             }
         } else {
             for (var i = 0; i < this.length; i++) {
-                this[i].className += ' ' + v;
-            }
-        }
-
-        return this;
-    },
-
-    removeClass: function(v) {
-        // Return false when v is '', void, undefined, false or when v is not a string
-        if (!v || typeof v !== 'string') {
-            return false;
-        }
-
-        if (document.body.classList) {
-            for (var i = 0; i < this.length; i++) {
-                this[i].classList.remove(v);
-            }
-        } else {
-            for (var i = 0; i < this.length; i++) {
-                this[i].className = this[i].className.replace(new RegExp('(^|\\b)' + v.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-            }
-        }
-
-        return this;
-    },
-
-    hasClass: function(v) {
-        var check;
-
-        // Return false when v is '', void, undefined, false or when v is not a string
-        if (!v || typeof v !== 'string') {
-            return false;
-        }
-
-        if (document.body.classList) {
-            for (var i = 0; i < this.length; i++) {
-                check = this[i].classList.contains(v);
-            }
-        } else {
-            for (var i = 0; i < this.length; i++) {
-                check = new RegExp('(^|)' + cls + '(|$)', 'gi').test(this[i].className);
-            }
-        }
-
-        if (check) {
-            return true;
-        } else {
-            return false;
-        }
-    },
-
-    toggleClass: function(v) {
-        // Return false when v is '', void, undefined, false or when v is not a string
-        if (!v || typeof v !== 'string') {
-            return false;
-        }
-
-        if (document.body.classList) {
-            for (var i = 0; i < this.length; i++) {
-                this[i].classList.toggle(v);
-            }
-        } else {
-            for (var i = 0; i < this.length; i++) {
-                if (this[i].className.indexOf(v) >= 0) {
-                    this[i].className = this[i].className.replace(new RegExp('(^|\\b)' + v.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-                } else {
-                    this[i].className += ' ' + v;
+                for (var event in this[i]) {
+                    if (object.hasOwnProperty(event)) {
+                        this[i].addEventListener(event, this[i][event]);
+                    }
                 }
             }
         }
     }
 });
-
 
 
 if (typeof module === "object" && typeof module.exports === "object") {
