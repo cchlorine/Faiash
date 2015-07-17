b02b385adb60e13249e3ae11811100c5ad4a4118
Rakume Hayashi <i@fake.moe>
2015-07-17 14:53:18 +0800

    Rename on to bind, add unbind

--- src/event.js
+++ src/event.js
@@ -5,7 +5,7 @@
  */
 
 Faiash.ise.extend({
-    on: function() {
+    bind: function() {
         if (typeof arguments[0] === 'string') {
             for (var i = 0; i < this.length; i++) {
                 this[i].addEventListener(arguments[0], arguments[1], arguments[2]);
@@ -15,6 +15,22 @@ Faiash.ise.extend({
                 for (var event in this[i]) {
                     if (object.hasOwnProperty(event)) {
                         this[i].addEventListener(event, this[i][event]);
+                    }
+                }
+            }
+        }
+    },
+
+    unbind: function() {
+        if (typeof arguments[0] === 'string') {
+            for (var i = 0; i < this.length; i++) {
+                this[i].removeEventListener(arguments[0], arguments[1], arguments[2]);
+            }
+        } else {
+            for (var i = 0; i < this.length; i++) {
+                for (var event in this[i]) {
+                    if (object.hasOwnProperty(event)) {
+                        this[i].removeEventListener(event, this[i][event]);
                     }
                 }
             }
