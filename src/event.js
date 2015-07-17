9c71afc8d8c6c3d9870a8cd8d6d78d70fc51add1
Rakume Hayashi <i@fake.moe>
2015-07-17 21:01:35 +0800

    Prevent the abort when no arguments

--- src/event.js
+++ src/event.js
@@ -9,8 +9,12 @@ Faiash.ise.extend({
         var args = $.toArr(arguments);
 
         if (typeof args[0] === 'string') {
+            if (!args[0] || !args[1]) {
+                return this;
+            }
+
             this.each(function(el) {
-                el.addEventListener(args[0], args[1], args[2]);
+                el.addEventListener(args[0], args[1], (args[2] ? args[2] : false));
             });
         } else if (typeof args[0] === 'object') {
             Faiash.each(args, function(obj) {
